8d7ff5df8fd39054859c63b2519afceff941ee18
andrewgepearce <andrew@pearce-family.org.uk>
2021-11-11 09:45:25 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import {getSortedPostsData} from "../lib/posts";
 
-export default function Home() {
+function getPostData(post) {
+	let r = <div>ID={post.id}</div>;
+	console.log(r);
+	return r;
+}
+
+function getPosts(postArray) {
+	console.log(postArray);
+	return postArray.map((e) => {
+		return getPostData(e);
+	});
+}
+
+export default function Home(props) {
+	console.log(props);
+	let p = getPosts(props.allPostsData.postArray);
+	console.log(p);
 	return (
 		<div className="container">
 			<Head>
@@ -15,10 +32,22 @@ export default function Home() {
 				<p className="description">
 					Get started by editing <code>pages/index.js</code>
 				</p>
-				<p>And here we go</p>
+				<p>And here we go:</p>
+
+				{p}
 			</main>
 
 			<Footer />
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const allPostsData = await getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+		revalidate: 10,
+	};
 }
